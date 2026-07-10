ab2757403c90963f0323fcbd901bb2e9ce923c80
KTajerbashi <kamrantajerbashi@gmail.com>
2026-07-10 20:09:39 +0330

    #Feature Add CancellationToken Console and WebApi For Test CancellationToken Logic in .net and Application

---
+++ CancellationToken/ConsoleApp_CancellationToken/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp_CancellationToken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+
+            CancellationToken token = cts.Token;
+
+            Task.Run(async () =>
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                Console.WriteLine($"{i} : {token.IsCancellationRequested} - {token.CanBeCanceled}");
+
+                    await Task.Delay(1000);
+
+                    if (token.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Canceled : {0}", token.ToString());
+                        return;
+                    }
+                }
+
+            }, token);
+
+            Console.ReadLine();
+
+            cts.Cancel();
+
+            Console.ReadLine();
+        }
+    }
+}
