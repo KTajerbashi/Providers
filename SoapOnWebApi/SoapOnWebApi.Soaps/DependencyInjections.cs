cec69092e8dab74b52897cfd7f228d0996b85103
KTajerbashi <kamrantajerbashi@gmail.com>
2026-07-18 21:49:28 +0330

    #Feature Add WCF Package

--- SoapOnWebApi/SoapOnWebApi.Soaps/DependencyInjections.cs
+++ SoapOnWebApi/SoapOnWebApi.Soaps/DependencyInjections.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using SoapOnWebApi.Soaps.Contracts;
+using SoapOnWebApi.Soaps.Services;
 
 namespace SoapOnWebApi.Soaps;
 
@@ -7,6 +9,7 @@ public static class DependencyInjections
 {
     public static IServiceCollection AddSoapLibrary(this IServiceCollection services)
     {
+        services.AddScoped<ICalculatorRepository, CalculatorRepository>();
         return services;
     }
     public static IApplicationBuilder UseSoapLibrary(this IApplicationBuilder app)
---
+++ SoapOnWebApi/SoapOnWebApi.Soaps/WebServices/ICalculatorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SoapOnWebApi.Soaps.WebServices
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculatorService" in both code and config file together.
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
